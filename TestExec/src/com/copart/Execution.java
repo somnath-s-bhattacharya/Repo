c078cce6cd37c6b40dc930f5e319f1fd1f10cb38
Somnath Bhattacharjee <somnath.s.bhattacharya@gmail.com>
2015-07-13 18:51:17 +0530

    Test Exec

---
+++ TestExec/src/com/copart/Execution.java
@@ -0,0 +1,98 @@
+package com.copart;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+
+public class Execution {
+	
+	static StringBuilder sb1 = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		
+		//store JSON Payload in string
+        BufferedReader br1 = new BufferedReader(new FileReader("InputJSON.txt"));
+	    try {   
+	        String line = br1.readLine();
+	        while (line != null) {
+	            sb1.append(line.trim());
+	            //sb1.append("\n");
+	            line = br1.readLine();
+	        }
+	        System.out.println("JSON stored in Payload StringBuilder");
+	    } 
+	    finally {
+	        br1.close();
+	    }
+	    
+	    HttpPost post = new HttpPost("http://iediws002.ied.copart.com:10090/assignment-ws/assignment/");
+	    post.setHeader("Content-type", "application/json");
+	    post.setHeader("Accept-Encoding", "gzip,deflate");
+	    post.setHeader("source", "WOM");
+	    post.setHeader("countryCode", "DE");
+	    post.setHeader("language", "en");
+	    post.setHeader("userEmail", "rachna.sharma@copart.com");
+	    post.setHeader("Content-Length", "1173");
+	    post.setHeader("Host", "iediws002.ied.copart.com:10090");
+	    post.setHeader("Connection", "Keep-Alive");
+	    post.setHeader("User-Agent", "Apache-HttpClient/4.1.1 (java 1.5)");
+	    
+	    post.setEntity(new StringEntity(sb1.toString(), "UTF-8"));
+	    try
+	    {
+	    HttpClient client = HttpClientBuilder.create().build();
+	    HttpResponse httpresponse = client.execute(post);
+	    HttpEntity entity = httpresponse.getEntity();
+	    InputStream stream = entity.getContent();
+	    String result = convertStreamToString(stream);
+	    System.out.println(result);
+	    }
+	    catch (ClientProtocolException e) {
+            // writing exception to log
+            e.printStackTrace();
+
+        }
+	    
+	}
+	
+	public static String convertStreamToString(InputStream is)
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try
+        {
+            while ((line = reader.readLine()) != null)
+            {
+                    sb.append(line + "\n");
+            } 
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                is.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+}
+
+}
--- TestExec/src/com/copart/JerseyClientPost.java
+++ TestExec/src/com/copart/JerseyClientPost.java
@@ -2,6 +2,9 @@ package com.copart;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+
+import javax.ws.rs.core.MediaType;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -15,15 +18,16 @@ public class JerseyClientPost {
     try {
 
         Client client = Client.create();
-        WebResource webResource = client.resource("http://iediws002.ied.copart.com:10090/assignment-ws/assignmentEntry/");
+        WebResource webResource = client.resource("http://iediws002.ied.copart.com:10090/assignment-ws/assignment/");
+        //webResource.header(
         
         //store JSON Payload in string
         BufferedReader br1 = new BufferedReader(new FileReader("InputJSON.txt"));
 	    try {   
 	        String line = br1.readLine();
 	        while (line != null) {
-	            sb1.append(line);
-	            sb1.append("\n");
+	            sb1.append(line.trim());
+	            //sb1.append("\n");
 	            line = br1.readLine();
 	        }
 	        System.out.println("JSON stored in Payload StringBuilder");
@@ -37,9 +41,14 @@ public class JerseyClientPost {
 	    try {   
 	        String line = br2.readLine();
 	        while (line != null) {
-	            sb2.append(line);
-	            sb2.append("\n");
-	            line = br2.readLine();
+	            //sb2.append(line);
+	            //sb2.append("\n");
+	        	
+	            
+	        	if(line.contains(":")){
+	        		webResource.setProperty(line.substring(0,  line.indexOf(":")), (line.substring(line.indexOf(":") + 1)).trim());
+	        		line = br2.readLine();
+	        	}
 	        }
 	        System.out.println("JSON stored in header StringBuilder");
 	    } 
@@ -47,18 +56,23 @@ public class JerseyClientPost {
 	        br2.close();
 	    }
 	    
+	    //String input_header = sb2.toString();
+	    //
+	    System.out.println(webResource.getProperties());
 	    
 	    String input_payload = sb1.toString();
-        String input_header = sb2.toString();
-        String input =  input_header+'\n' +input_payload;
-        System.out.println(input);
+        System.out.println(input_payload);
+       // String input =  input_header+'\n' +input_payload;
+        //System.out.println(input);
         
-		ClientResponse response = webResource.type("application/json").post(ClientResponse.class, input);
-		
+        ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, input_payload);
+        //System.out.println(response);
+		/*
         if (response.getStatus() != 400) 
         {
             throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
         }
+        */
 
         System.out.println("Output from Server .... \n");
         String output = response.toString();
